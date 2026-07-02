99f30dc2b172215af8c42e5247be40d8c38df271
Lilias-Olrend <lilias0346@gmail.com>
2026-07-02 10:25:22 +0900

    wa!

--- lific.c
+++ lific.c
@@ -144,6 +144,7 @@ Token *tokenize() {
 Node *expr(void);
 Node *mul(void);
 Node *primary(void);
+Node *unary(void);
 
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
@@ -175,16 +176,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
 }
 
 void gen(Node *node) {
--- tmp.s
+++ tmp.s
@@ -1,17 +1,11 @@
 .intel_syntax noprefix
 .globl main
 main:
-  push 3
+  push 0
   push 5
   pop rdi
   pop rax
-  add rax, rdi
-  push rax
-  push 2
-  pop rdi
-  pop rax
-  cqo
-  idiv rdi
+  sub rax, rdi
   push rax
   pop rax
   ret
